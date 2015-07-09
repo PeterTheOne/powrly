23e3c7cdf4ea3aef37753338c36f8dec02265342
PeterTheOne <petertheone@gmail.com>
2015-07-09 02:19:16 +0200

    more work done on header

--- wp-content/themes/powrly/templates/header.php
+++ wp-content/themes/powrly/templates/header.php
@@ -1,9 +1,5 @@
 <header class="banner" role="banner">
   <div class="container">
-    <div class="brand" href="<?= esc_url(home_url('/')); ?>">
-      <h1><?php bloginfo('name'); ?></h1>
-      <span class="tagline"><?php bloginfo ('description'); ?></span>
-    </div>
     <nav role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
@@ -11,5 +7,15 @@
       endif;
       ?>
     </nav>
+    <form>
+      <input class="search" type="text" placeholder="search" />
+    </form>
+    <div class="login">
+      <a href="">login</a>
+    </div>
+  </div>
+  <div class="brand" href="<?= esc_url(home_url('/')); ?>">
+    <h1><?php bloginfo('name'); ?></h1>
+    <span class="tagline"><?php bloginfo ('description'); ?></span>
   </div>
 </header>
